cc572f3342abf2ba16e44335cd9b4cbcc3bd940c
NickSot <nikola.s.sotirov@gmail.com>
2019-07-24 01:06:10 +0300

    loopback ip to db

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '192.168.10.10'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'GuardCityDB'),
             'username' => env('DB_USERNAME', 'root'),
