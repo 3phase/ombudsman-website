1f7756ec1e6e7f90fd9d9b1f13ba05652bcbbffa
NickSot <nikola.s.sotirov@gmail.com>
2019-07-23 21:14:56 +0300

    added a different ip to db

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '192.168.10.10'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'GuardCityDB'),
             'username' => env('DB_USERNAME', 'root'),
