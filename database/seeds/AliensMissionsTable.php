9b023605608922795823366d1874ec6bfe315323
NickSot <nikola.s.sotirov@gmail.com>
2019-07-30 13:54:27 +0300

    Adding one more mission...

--- database/seeds/AliensMissionsTable.php
+++ database/seeds/AliensMissionsTable.php
@@ -13,7 +13,14 @@ class AliensMissionsTable extends Seeder
     {
         DB::table('aliens_missions')->insert([
             'alien_id' => 1,
-            'node_id' => 1
+            'node_id' => 1,
+            'reachable_population' => 0
+        ]);
+
+        Db::table('aliens_missions')->insert([
+            'alien_id' => 2,
+            'node_id' => 5,
+            'reachable_population' => 1
         ]);
     }
 }
