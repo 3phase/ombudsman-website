a7b3d2262d28a87a68dda89ac4d4bfdd96aff02e
NickSot <nikola.s.sotirov@gmail.com>
2019-09-13 17:28:00 +0300

    Adding image file name to the planets in the seeds

--- database/seeds/PlanetsTableSeeder.php
+++ database/seeds/PlanetsTableSeeder.php
@@ -13,18 +13,21 @@ class PlanetsTableSeeder extends Seeder
     {
         DB::table('planets')->insert([
             'name' => 'Root35E2',
+            'image_filename' => 'planet1.jpg',
             'level' => 1,
             'unlocking_popularity' => 0 
         ]);
         
         DB::table('planets')->insert([
             'name' => 'El',
+            'image_filename' => 'planet2.jpg',
             'level' => 3,
             'unlocking_popularity' => 100 
         ]);
 
         DB::table('planets')->insert([
             'name' => 'Esteban',
+            'image_filename' => 'planet3.jpg',
             'level' => 2,
             'unlocking_popularity' => 5 
         ]);
