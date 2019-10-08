9b692bbecf59047d2150af5e241b44ae64378fbb
NickSot <nikola.s.sotirov@gmail.com>
2019-10-08 22:46:59 +0300

    Started migrating logic to AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,4 +19,21 @@ class AuthController extends Controller
             'token' => $token
         ], 200);
     }
+
+    public function getProgress(){
+        $user = \App\User::find(\Cookie::get('user_id'));
+        return response()->json($user->player()->first()->progress()->first());
+    }
+
+    public function getAlien($id){
+        return response()->json(\App\Alien::find($id));
+    }
+
+    public function getPlanet($id){
+        $planet = \App\Planet::find($id);
+        $aliens = \App\Alien::where('alien_id', $id);
+        unset($planet->created_at);
+        unset($planet->updated_at);
+        return response()->json($planet->name, $aliens);
+    }
 }
